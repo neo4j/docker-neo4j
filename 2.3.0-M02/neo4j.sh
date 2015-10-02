ce1bb4ef8e726792a9261860c201b47b7ce9daee
Ben Butler-Cole <ben@bridesmere.com>
2015-10-02 10:04:51 +0000

    Remove duplication of Neo4j install path

--- 2.3.0-M02/neo4j.sh
+++ 2.3.0-M02/neo4j.sh
@@ -1,34 +1,30 @@
 #!/bin/bash -eu
 
-NEO4J_HOME=/var/lib/neo4j
-
 if [ -n "${NEO4J_OPEN_FILES:-}" ]; then
     ulimit -n $NEO4J_OPEN_FILES >/dev/null
 fi
 
-# NEO4J_HEAP_MEMORY=2G
 if [ -n "${NEO4J_HEAP_MEMORY:-}" ]; then
-    echo "wrapper.java.additional=-Xmx${NEO4J_HEAP_MEMORY}" >> $NEO4J_HOME/conf/neo4j-wrapper.conf
-    echo "wrapper.java.additional=-Xms${NEO4J_HEAP_MEMORY}" >> $NEO4J_HOME/conf/neo4j-wrapper.conf
+    echo "wrapper.java.additional=-Xmx${NEO4J_HEAP_MEMORY}" >> conf/neo4j-wrapper.conf
+    echo "wrapper.java.additional=-Xms${NEO4J_HEAP_MEMORY}" >> conf/neo4j-wrapper.conf
 fi
 
-# NEO4J_CACHE_MEMORY=2G
 if [ -n "${NEO4J_CACHE_MEMORY:-}" ]; then
-    sed -i -e "s|.*dbms.pagecache.memory=.*|dbms.pagecache.memory=${NEO4J_CACHE_MEMORY}|g" $NEO4J_HOME/conf/neo4j.properties
+    sed -i -e "s|.*dbms.pagecache.memory=.*|dbms.pagecache.memory=${NEO4J_CACHE_MEMORY}|g" conf/neo4j.properties
 fi
 
 if [ ! -z ${NEO4J_NO_AUTH+x} ]; then
-    sed -i -e "s|dbms.security.auth_enabled=.*|dbms.security.auth_enabled=false|g" $NEO4J_HOME/conf/neo4j-server.properties
+    sed -i -e "s|dbms.security.auth_enabled=.*|dbms.security.auth_enabled=false|g" conf/neo4j-server.properties
 fi
 
 if [ ! -z ${NEO4J_UDC_SOURCE+x} ]; then
-    sed -i -e "s|Dneo4j.ext.udc.source=.*|Dneo4j.ext.udc.source=${NEO4J_UDC_SOURCE}|g" $NEO4J_HOME/conf/neo4j-wrapper.conf
+    sed -i -e "s|Dneo4j.ext.udc.source=.*|Dneo4j.ext.udc.source=${NEO4J_UDC_SOURCE}|g" conf/neo4j-wrapper.conf
 fi
 
 if [ -d /conf ]; then
-    cp -R /conf/ $NEO4J_HOME/conf/
+    cp -R /conf/ conf/
 fi
 
-sed -i "s|#org.neo4j.server.webserver.address=0.0.0.0|org.neo4j.server.webserver.address=$HOSTNAME|g" $NEO4J_HOME/conf/neo4j-server.properties
+sed -i "s|#org.neo4j.server.webserver.address=0.0.0.0|org.neo4j.server.webserver.address=$HOSTNAME|g" conf/neo4j-server.properties
 
-exec neo4j console
+exec bin/neo4j console
