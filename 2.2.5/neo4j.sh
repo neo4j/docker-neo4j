2f22a36230e2576fb7ed9566b01f464a0358872d
Ben Butler-Cole <ben@bridesmere.com>
2015-10-02 12:03:15 +0000

    Move configuration defaults to neo4j.sh to remove duplication

--- 2.2.5/neo4j.sh
+++ 2.2.5/neo4j.sh
@@ -9,17 +9,15 @@ if [ -n "${NEO4J_HEAP_MEMORY:-}" ]; then
     echo "wrapper.java.additional=-Xms${NEO4J_HEAP_MEMORY}" >>conf/neo4j-wrapper.conf
 fi
 
-if [ -n "${NEO4J_CACHE_MEMORY:-}" ]; then
-    sed --in-place "s|.*dbms.pagecache.memory=.*|dbms.pagecache.memory=${NEO4J_CACHE_MEMORY}|g" conf/neo4j.properties
-fi
+sed --in-place "s|.*dbms.pagecache.memory=.*|dbms.pagecache.memory=${NEO4J_CACHE_MEMORY:-512M}|g" conf/neo4j.properties
 
 if [ -n "${NEO4J_NO_AUTH:-}" ]; then
     sed --in-place "s|dbms.security.auth_enabled=.*|dbms.security.auth_enabled=false|g" conf/neo4j-server.properties
 fi
 
-if [ -n "${NEO4J_UDC_SOURCE:-}" ]; then
-    sed --in-place "s|Dneo4j.ext.udc.source=.*|Dneo4j.ext.udc.source=${NEO4J_UDC_SOURCE}|g" conf/neo4j-wrapper.conf
-fi
+sed --in-place "s|Dneo4j.ext.udc.source=.*|Dneo4j.ext.udc.source=${NEO4J_UDC_SOURCE:-docker}|g" conf/neo4j-wrapper.conf
+sed --in-place "s|.*keep_logical_logs=.*|keep_logical_logs=100M size|g" conf/neo4j.properties
+sed --in-place "s|#*remote_shell_enabled=.*|remote_shell_enabled=true|g" conf/neo4j.properties
 
 if [ -d /conf ]; then
     cp --recursive /conf/ conf/
