b994efbd61441b5cd275e765443a439f92ba390e
Ben Butler-Cole <ben@bridesmere.com>
2015-10-07 15:36:30 +0000

    Factor out duplication modifying configuration

--- neo4j.sh
+++ neo4j.sh
@@ -1,23 +1,27 @@
 #!/bin/bash -eu
 
+setting() {
+    setting="${1}"
+    value="${2}"
+    file="${3}"
+    if [ -n "${value}" ]; then
+        sed --in-place "s|.*${setting}=.*|${setting}=${value}|" conf/"${file}"
+    fi
+}
+
 if [ -d /conf ]; then
     rm --recursive --force conf
     ln --symbolic /conf
 else
-    sed --in-place "s|#org.neo4j.server.webserver.address=0.0.0.0|org.neo4j.server.webserver.address=0.0.0.0|g" conf/neo4j-server.properties
-
-    sed --in-place "s|.*keep_logical_logs=.*|keep_logical_logs=${NEO4J_KEEP_LOGICAL_LOGS:-100M size}|g" conf/neo4j.properties
-    sed --in-place "s|.*dbms.pagecache.memory=.*|dbms.pagecache.memory=${NEO4J_CACHE_MEMORY:-512M}|g" conf/neo4j.properties
-    sed --in-place "s|Dneo4j.ext.udc.source=.*|Dneo4j.ext.udc.source=${NEO4J_UDC_SOURCE:-docker}|g" conf/neo4j-wrapper.conf
-
-    if [ -n "${NEO4J_HEAP_MEMORY:-}" ]; then
-        echo "wrapper.java.additional=-Xmx${NEO4J_HEAP_MEMORY}" >>conf/neo4j-wrapper.conf
-        echo "wrapper.java.additional=-Xms${NEO4J_HEAP_MEMORY}" >>conf/neo4j-wrapper.conf
-    fi
+    setting "org.neo4j.server.webserver.address" "0.0.0.0" neo4j-server.properties
+    setting "keep_logical_logs" "${NEO4J_KEEP_LOGICAL_LOGS:-100M size}" neo4j.properties
+    setting "dbms.pagecache.memory" "${NEO4J_CACHE_MEMORY:-512M}" neo4j.properties
+    setting "wrapper.java.additional=-Dneo4j.ext.udc.source" "${NEO4J_UDC_SOURCE:-docker}" neo4j-wrapper.conf
+    setting "wrapper.java.initmemory" "${NEO4J_HEAP_MEMORY:-}" neo4j-wrapper.conf
+    setting "wrapper.java.maxmemory" "${NEO4J_HEAP_MEMORY:-}" neo4j-wrapper.conf
 
     if [ "${NEO4J_AUTH:-}" == "none" ]; then
-        sed --in-place "s|dbms.security.auth_enabled=.*|dbms.security.auth_enabled=false|g" \
-            conf/neo4j-server.properties
+        setting "dbms.security.auth_enabled" "false" neo4j-server.properties
     elif [[ "${NEO4J_AUTH:-}" == neo4j/* ]]; then
         password="${NEO4J_AUTH#neo4j/}"
         bin/neo4j start || \
