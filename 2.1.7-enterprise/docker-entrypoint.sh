6158723bdd85f72a59ddb5172d60a906c28a0bcd
Adrian Ursu <Adrian.Ursu@opencredo.com>
2015-10-28 11:20:18 +0000

    Removed settings unavailable for 2.1.7 edition.

--- 2.1.7-enterprise/docker-entrypoint.sh
+++ 2.1.7-enterprise/docker-entrypoint.sh
@@ -15,29 +15,10 @@ if [ "$1" == "neo4j" ]; then
         ln --symbolic /conf
     else
         setting "keep_logical_logs" "${NEO4J_KEEP_LOGICAL_LOGS:-100M size}" neo4j.properties
-        setting "dbms.pagecache.memory" "${NEO4J_CACHE_MEMORY:-512M}" neo4j.properties
         setting "wrapper.java.additional=-Dneo4j.ext.udc.source" "${NEO4J_UDC_SOURCE:-docker}" neo4j-wrapper.conf
         setting "wrapper.java.initmemory" "${NEO4J_HEAP_MEMORY:-512}" neo4j-wrapper.conf
         setting "wrapper.java.maxmemory" "${NEO4J_HEAP_MEMORY:-512}" neo4j-wrapper.conf
         setting "org.neo4j.server.thirdparty_jaxrs_classes" "${NEO4J_THIRDPARTY_JAXRS_CLASSES:-}" neo4j-server.properties
-
-        if [ "${NEO4J_AUTH:-}" == "none" ]; then
-            setting "dbms.security.auth_enabled" "false" neo4j-server.properties
-        elif [[ "${NEO4J_AUTH:-}" == neo4j/* ]]; then
-            password="${NEO4J_AUTH#neo4j/}"
-            bin/neo4j start || \
-                (cat data/log/console.log && echo "Neo4j failed to start" && exit 1)
-            if ! curl --fail --silent --user "neo4j:${password}" http://localhost:7474/db/data/ >/dev/null ; then
-                curl --fail --silent --show-error --user neo4j:neo4j \
-                     --data '{"password": "'"${password}"'"}' \
-                     --header 'Content-Type: application/json' \
-                     http://localhost:7474/user/neo4j/password
-            fi
-            bin/neo4j stop
-        elif [ -n "${NEO4J_AUTH:-}" ]; then
-            echo "Invalid value for NEO4J_AUTH: '${NEO4J_AUTH}'"
-            exit 1
-        fi
 
         setting "org.neo4j.server.webserver.address" "0.0.0.0" neo4j-server.properties
         setting "org.neo4j.server.database.mode" "${NEO4J_DATABASE_MODE:-}" neo4j-server.properties
