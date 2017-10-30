2052e969099da2e58bccdc25ce543dafdb4c9c94
joconnor <joconnor@franklinamerican.com>
2017-10-30 13:26:12 -0500

    #94 Allow auth credentials to be configured via files.

--- src/3.0/docker-entrypoint.sh
+++ src/3.0/docker-entrypoint.sh
@@ -20,6 +20,28 @@ setting() {
     fi
 }
 
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
+# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
+#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+file_env() {
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+		exit 1
+	fi
+	local val="$def"
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	elif [ "${!fileVar:-}" ]; then
+		val="$(< "${!fileVar}")"
+	fi
+	export "$var"="$val"
+	unset "$fileVar"
+}
+
 if [ "$1" == "neo4j" ]; then
 
     # Env variable naming convention:
@@ -77,6 +99,7 @@ if [ "$1" == "neo4j" ]; then
         NEO4J_dbms_directories_metrics="/metrics"
     fi
 
+    file_env 'NEO4J_AUTH'
     if [ "${NEO4J_AUTH:-}" == "none" ]; then
         NEO4J_dbms_security_auth__enabled=false
     elif [[ "${NEO4J_AUTH:-}" == neo4j/* ]]; then
