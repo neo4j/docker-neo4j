be1c1b8d1a732b7969b550036ee3f0e8faca0491
Jenny <jenny.owen@neo4j.com>
2019-05-09 12:34:40 +0200

    fixed chowning of mounted folders.

--- src/3.5/docker-entrypoint.sh
+++ src/3.5/docker-entrypoint.sh
@@ -10,32 +10,71 @@ function running_as_root
 function is_not_writable
 {
     _file=${1}
-    echo "File ${_file} owner stats: $(stat -c %U ${_file}):$(stat -c %G ${_file}) and $(stat -c %u ${_file}):$(stat -c %g ${_file})"
-    echo "comparing to ${userid}:${groupid}"
+#    echo "File ${_file} owner stats: $(stat -c %U ${_file}):$(stat -c %G ${_file}) and $(stat -c %u ${_file}):$(stat -c %g ${_file})"
+#    echo "comparing to ${userid}:${groupid}"
     test "$(stat -c %U ${_file})" != "${userid}"  &&  \
     test "$(stat -c %u ${_file})" != "${userid}"
 }
 
-function check_write_or_fail ()
+function print_permissions_advice_and_fail ()
 {
-  _directory=${1}
-  if [[ ! -d "${_directory}" ]]; then
-    echo >&2 "Not a directory: ${_directory}"
-    exit 1
-  fi
+    _directory=${1}
+    echo >&2 "
+Folder ${_directory} is not writable for user: ${userid} or group ${groupid}, this is commonly a file permissions issue on the mounted folder.
 
-  if is_not_writable "${_directory}"; then
-       echo >&2 "
-Folder ${_directory} is not writable for user: ${userid} or group ${groupid}, this looks like a file permissions
-issue on the mounted folder.
-
-Hint to solve the issue:
-chown -R ${userid}:${groupid} /path/to/mount_dir
-sudo find /path/to/mount_dir -type d -exec chmod 750 {} \";\"
-sudo find /path/to/mount_dir -type f -exec chmod 640 {} \";\"
+Hints to solve the issue:
+1) Make sure the folder exists before mounting it. Docker will create the folder using root permissions before starting the Neo4j container. The root permissions disallow Neo4j from writing to the mounted folder.
+2) Pass the folder owner's user ID and group ID to docker run, so that docker runs as that user.
+This can be done by adding this flag to your docker run command:
+  --user=\$(id -u):\$(id -g)
        "
-       exit 1
-  fi
+    exit 1
+}
+
+function check_mounted_folder
+{
+    _directory=${1}
+    if is_not_writable "${_directory}"; then
+        print_permissions_advice_and_fail "${_directory}"
+    fi
+}
+
+function check_mounted_folder_with_chown
+{
+# The /data and /log directory are a bit different because they are very likely to be mounted by the user but not
+# necessarily writable.
+# This depends on whether a user ID is passed to the container and which folders are mounted.
+#
+#   No user ID passed to container:
+#   1) No folders are mounted.
+#      The /data and /log folder are owned by neo4j by default, so should be writable already.
+#   2) Both /log and /data are mounted.
+#      This means on start up, /data and /logs are owned by an unknown user and we should chown them to neo4j for
+#      backwards compatibility.
+#
+#   User ID passed to container:
+#   1) Both /data and /logs are mounted
+#      The /data and /logs folders are owned by an unknown user but we *should* have rw permission to them.
+#      That should be verified and error (helpfully) if not.
+#   2) User mounts /data or /logs *but not both*
+#      The  unmounted folder is still owned by neo4j, which should already be writable. The mounted folder should
+#      have rw permissions through user id. This should be verified.
+#   4) No folders are mounted.
+#      The /data and /log folder are owned by neo4j by default, and these are already writable by the user.
+#      (This is a very unlikely use case).
+
+    mountFolder=${1}
+    if running_as_root; then
+        if is_not_writable "${mountFolder}"; then
+            # warn that we're about to chown the /data folder and then chown it
+            echo >&2 "Warning: Folder mounted to \"${mountFolder}\" is not writable from inside container. Changing folder owner to ${userid}."
+            chown -R "${userid}":"${groupid}" "${mountFolder}"
+        fi
+    else
+        if [ ! -w "${mountFolder}" ]  && [[ "$(stat -c %U ${mountFolder})" != "neo4j" ]]; then
+            print_permissions_advice_and_fail "${mountFolder}"
+        fi
+    fi
 }
 
 # If we're running as root, then run as the neo4j user. Otherwise
@@ -59,25 +98,25 @@ readonly exec_cmd
 # volume here (notably a conf volume). So take care not to chown
 # volumes (stuff not owned by neo4j)
 if running_as_root; then
-  # Non-recursive chown for the base directory
-  chown "${userid}":"${groupid}" "${NEO4J_HOME}"
-  chmod 700 "${NEO4J_HOME}"
-    #  find "${NEO4J_HOME}" -type d -mindepth 1 -maxdepth 1 -exec chown -R ${userid}:${groupid} {} \;
-    #  find "${NEO4J_HOME}" -type d -mindepth 1 -maxdepth 1 -exec chmod 700 {} \;
+    # Non-recursive chown for the base directory
+    chown "${userid}":"${groupid}" "${NEO4J_HOME}"
+    chmod 700 "${NEO4J_HOME}"
+    find "${NEO4J_HOME}" -type d -mindepth 1 -maxdepth 1 -user root -exec chown -R ${userid}:${groupid} {} \;
+    find "${NEO4J_HOME}" -type d -mindepth 1 -maxdepth 1 -user root -exec chmod 700 {} \;
 fi
 
-while IFS= read -r -d '' dir
-do
-  if running_as_root && [[ "$(stat -c %U "${dir}")" = "neo4j" ]]; then
-    # Using mindepth 1 to avoid the base directory here so recursive is OK
-    chown -R "${userid}":"${groupid}" "${dir}"
-    chmod -R 700 "${dir}"
-  fi
-done <   <(find "${NEO4J_HOME}" -type d -mindepth 1 -maxdepth 1 -print0)
+#while IFS= read -r -d '' dir
+#do
+#  if running_as_root && [[ "$(stat -c %U "${dir}")" = "neo4j" ]]; then
+#    # Using mindepth 1 to avoid the base directory here so recursive is OK
+#    chown -R "${userid}":"${groupid}" "${dir}"
+#    chmod -R 700 "${dir}"
+#  fi
+#done <   <(find "${NEO4J_HOME}" -type d -mindepth 1 -maxdepth 1 -print0)
 
 
 if [ "${cmd}" == "dump-config" ]; then
-  check_write_or_fail "/conf"
+  check_mounted_folder "/conf"
   ${exec_cmd} cp --recursive "${NEO4J_HOME}"/conf/* /conf
   exit 0
 fi
@@ -172,46 +211,39 @@ unset NEO4J_dbms_txLog_rotation_retentionPolicy NEO4J_UDC_SOURCE \
 : ${NEO4J_causal__clustering_raft__advertised__address:=$(hostname):7000}
 
 if [ -d /conf ]; then
-    check_write_or_fail "/conf"
+    check_mounted_folder "/conf"
     find /conf -type f -exec cp {} "${NEO4J_HOME}"/conf \;
 fi
 
 if [ -d /ssl ]; then
-    check_write_or_fail "/ssl"
+    check_mounted_folder "/ssl"
     NEO4J_dbms_directories_certificates="/ssl"
 fi
 
 if [ -d /plugins ]; then
-    check_write_or_fail "/plugins"
+    check_mounted_folder "/plugins"
     NEO4J_dbms_directories_plugins="/plugins"
 fi
 
 if [ -d /import ]; then
-    check_write_or_fail "/import"
+    check_mounted_folder "/import"
     NEO4J_dbms_directories_import="/import"
 fi
 
 if [ -d /metrics ]; then
-    check_write_or_fail "/metrics"
+    check_mounted_folder "/metrics"
     NEO4J_dbms_directories_metrics="/metrics"
 fi
 
-if [ -d /data ]; then
-    if is_not_writable "/data"; then
-        # warn that we're about to chown the /data folder and then chown it
-        echo >&2 "Folder mounted to \"/data\" is not writable from inside container."
-        chown -R "${userid}":"${groupid}" /data
-    fi
+if [ -d /logs ]; then
+    check_mounted_folder_with_chown "/logs"
+    NEO4J_dbms_directories_logs="/logs"
 fi
 
-if [ -d /logs ]; then
-    NEO4J_dbms_directories_logs="/logs"
-    if is_not_writable "/logs"; then
-        # warn that we're about to chown the /data folder and then chown it
-        echo >&2 "Folder mounted to \"/logs\" is not writable from inside container."
-        chown -R "${userid}":"${groupid}" /logs
-    fi
+if [ -d /data ]; then
+    check_mounted_folder_with_chown "/data"
 fi
+
 
 # set the neo4j initial password only if you run the database server
 if [ "${cmd}" == "neo4j" ]; then
