fcc1c076d84b4e965d78cded58311a82c6357782
Jenny <jenny.owen@neo4j.com>
2019-06-04 12:44:21 +0200

    made diagnostic message on logs folder check better

--- src/test/java/com/neo4j/docker/utils/TestSettings.java
+++ src/test/java/com/neo4j/docker/utils/TestSettings.java
@@ -8,8 +8,8 @@ import java.nio.file.Paths;
 public class TestSettings
 {
 //    public static final Neo4jVersion NEO4J_VERSION = Neo4jVersion.fromVersionString( System.getenv( "NEO4J_VERSION" ) );
-    public static final Neo4jVersion NEO4J_VERSION = Neo4jVersion.fromVersionString( "4.0.0-alpha07" );
-    public static final String IMAGE_ID = "test/27608:latest";
+    public static final Neo4jVersion NEO4J_VERSION = Neo4jVersion.fromVersionString( "3.5.7" );
+    public static final String IMAGE_ID = "test/18511:latest";
     public static final Path TEST_TMP_FOLDER = Paths.get("tmp", "local-mounts");
 //    public static final Path TEST_TMP_FOLDER = Paths.get("/","tmp");
     public static final Edition EDITION = Edition.ENTERPRISE;
