c002601bac96954d163e3944afc419692855b51c
Jenny <jenny.owen@neo4j.com>
2019-06-04 16:34:25 +0200

    can now run java tests from makefile and/or maven

--- src/test/java/com/neo4j/docker/utils/TestSettings.java
+++ src/test/java/com/neo4j/docker/utils/TestSettings.java
@@ -1,5 +1,8 @@
 package com.neo4j.docker.utils;
 
+import org.junit.Assert;
+import org.junit.runners.Parameterized;
+
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -7,16 +10,55 @@ import java.nio.file.Paths;
 // TODO fail test immediately and error if these values aren't set.
 public class TestSettings
 {
-//    public static final Neo4jVersion NEO4J_VERSION = Neo4jVersion.fromVersionString( System.getenv( "NEO4J_VERSION" ) );
-    public static final Neo4jVersion NEO4J_VERSION = Neo4jVersion.fromVersionString( "3.5.7" );
-    public static final String IMAGE_ID = "test/18511:latest";
+    public static final Neo4jVersion NEO4J_VERSION = Neo4jVersion.fromVersionString( getVersionFromPropertyOrEnv() );
+    public static final String IMAGE_ID = getImageFromPropertyOrEnv();
     public static final Path TEST_TMP_FOLDER = Paths.get("tmp", "local-mounts");
-//    public static final Path TEST_TMP_FOLDER = Paths.get("/","tmp");
-    public static final Edition EDITION = Edition.ENTERPRISE;
+    public static final Edition EDITION = getEditionFromPropertyOrEnv();
 
     public enum Edition
     {
         COMMUNITY,
         ENTERPRISE;
+    }
+
+    private static String getVersionFromPropertyOrEnv()
+    {
+        String verStr = System.getProperty( "version" );
+        if(verStr == null)
+        {
+            verStr = System.getenv( "NEO4J_VERSION" );
+        }
+        Assert.assertNotNull("Neo4j version has not been specified, either use mvn argument -Dversion or set env NEO4J_VERSION", verStr);
+        return verStr;
+    }
+
+    private static String getImageFromPropertyOrEnv()
+    {
+        String image = System.getProperty( "image" );
+        if(image == null)
+        {
+            image = System.getenv( "NEO4J_IMAGE" );
+        }
+        Assert.assertNotNull("Neo4j version has not been specified, either use mvn argument -Dimage or set env NEO4J_IMAGE", image);
+        return image;
+    }
+
+    private static Edition getEditionFromPropertyOrEnv()
+    {
+        String edition = System.getProperty( "edition" );
+        if(edition == null)
+        {
+            edition = System.getenv( "NEO4J_EDITION" );
+        }
+        switch ( edition.toLowerCase() )
+        {
+        case "community":
+            return Edition.COMMUNITY;
+        case "enterprise":
+            return Edition.ENTERPRISE;
+        default:
+            Assert.fail( "Neo4j version has not been specified, either use mvn argument -Dedition or set env NEO4J_EDITION" );
+        }
+        return null;
     }
 }
