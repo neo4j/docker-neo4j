94364bc72d80178e30ddfbc46c5fdf489542e2bc
Jenny <jenny.owen@neo4j.com>
2019-06-24 12:53:26 +0200

    HA test now working

--- src/test/java/com/neo4j/docker/TestHACluster.java
+++ src/test/java/com/neo4j/docker/TestHACluster.java
@@ -4,12 +4,14 @@ import com.neo4j.docker.utils.Neo4jVersion;
 import com.neo4j.docker.utils.SetContainerUser;
 import com.neo4j.docker.utils.TestSettings;
 import org.junit.Ignore;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.DockerComposeContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.containers.wait.strategy.WaitStrategy;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -17,11 +19,46 @@ import java.nio.file.Paths;
 import java.time.Duration;
 import java.util.Random;
 
+import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.GraphDatabase;
+import org.neo4j.driver.Session;
+import org.neo4j.driver.StatementResult;
+
 @Ignore
 public class TestHACluster
 {
     private Random rng = new Random(  );
     private static Logger log = LoggerFactory.getLogger( TestHACluster.class);
+    private String dbPassword = "neo";
+
+    private void putInitialDataIntoContainer( String boltUri )
+    {
+        Driver driver = GraphDatabase.driver( boltUri, AuthTokens.basic( "neo4j", dbPassword));
+        try ( Session session = driver.session())
+        {
+            StatementResult rs = session.run( "CREATE (arne:dog {name:'Arne'})-[:SNIFFS]->(bosse:dog {name:'Bosse'}) RETURN arne.name");
+            Assertions.assertEquals( "Arne", rs.single().get( 0 ).asString(), "did not receive expected result from cypher CREATE query" );
+        }
+        driver.close();
+    }
+
+    private void verifyDataInContainer( String boltUri )
+    {
+        Driver driver = GraphDatabase.driver( boltUri, AuthTokens.basic( "neo4j", dbPassword));
+        try ( Session session = driver.session())
+        {
+            StatementResult rs = session.run( "MATCH (a:dog)-[:SNIFFS]->(b:dog) RETURN a.name");
+            Assertions.assertEquals( "Arne", rs.single().get( 0 ).asString(), "did not receive expected result from cypher CREATE query" );
+        }
+        driver.close();
+    }
+
+    private String getBoltUriForService(DockerComposeContainer container, String service)
+    {
+        return "bolt://" + container.getServiceHost( service, 7687 ) +
+               ":" + container.getServicePort( service, 7687 );
+    }
 
     @Test
     void testHAStartsOK() throws Exception
@@ -33,22 +70,17 @@ public class TestHACluster
         Assumptions.assumeTrue( TestSettings.NEO4J_VERSION.isNewerThan( new Neo4jVersion( 3,0,0 )),
                                 "HA Tests don't apply before 3.0");
 
-        String testID = String.format( "%04d", rng.nextInt(10000 ) );
-        Path logDir = TestSettings.TEST_TMP_FOLDER.resolve( "HA_cluster_" + testID );
+        Path logDir = TestSettings.TEST_TMP_FOLDER.resolve( String.format( "HA_cluster_%04d", rng.nextInt(10000 ) ));
         log.info( "writing HA test logs into "+logDir.toString() );
 
         Path composeTemplate = Paths.get( "src", "test", "resources", "ha-cluster-compose.yml" );
         Path composeFile = logDir.resolve( "ha-cluster-compose.yml" );
-        String masterContainer = String.format( "master_" + testID );
-        String slaveContainer = String.format( "slave_" + testID );
 
         // read the HA compose file template and replace placeholders
         String composeContent = new String( Files.readAllBytes( composeTemplate ) );
         composeContent = composeContent
                 .replaceAll( "%%USERIDGROUPID%%", SetContainerUser.getCurrentlyRunningUserString() )
                 .replaceAll( "%%IMAGE%%", TestSettings.IMAGE_ID )
-                .replaceAll( "%%MASTER_CONTAINER%%", masterContainer )
-                .replaceAll( "%%SLAVE_CONTAINER%%", slaveContainer )
                 .replaceAll( "%%LOGS_DIR%%", logDir.toAbsolutePath().toString() );
 
         // create log folders
@@ -60,13 +92,21 @@ public class TestHACluster
         Files.write( composeFile, composeContent.getBytes() );
 
         // now actually start the cluster
-        DockerComposeContainer clusteringContainer = new DockerComposeContainer( "HA_cluster_"+testID, composeFile.toFile() )
+        WaitStrategy waiter = Wait.forListeningPort().withStartupTimeout( Duration.ofSeconds( 90 ) );
+        DockerComposeContainer clusteringContainer = new DockerComposeContainer( composeFile.toFile() )
                 .withLocalCompose(true)
                 .withExposedService( "master", 7687 )
-                .waitingFor( "master", Wait.forListeningPort().withStartupTimeout( Duration.ofSeconds( 90 ) ));
+                .withExposedService( "slave1", 7687 )
+                .waitingFor( "master", waiter)
+                .waitingFor( "slave1", waiter);
         clusteringContainer.start();
+
         // write some data
+        log.info( "Cluster started, writing data to master" );
+        putInitialDataIntoContainer( getBoltUriForService( clusteringContainer, "master" ) );
 
         // read some data
+        log.info( "Reading data from slave" );
+        verifyDataInContainer( getBoltUriForService( clusteringContainer, "slave1" ) );
     }
 }
