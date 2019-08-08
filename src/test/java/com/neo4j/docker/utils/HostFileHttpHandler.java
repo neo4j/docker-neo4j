7d7f92eb134b24a3809d21213987755ec86d79a4
Andrew Jefferson <andrew.jefferson@neotechnology.com>
2019-08-08 21:26:29 +0000

    Revert "Revert "Merge pull request #181 from eastlondoner/andy/install-apoc-at-runtime""
    This reverts commit 84425d99fd10ab50f05a59bfb9ca79ec0ec7ed05.
    
    Which re-enables plugin installation at runtime

---
+++ src/test/java/com/neo4j/docker/utils/HostFileHttpHandler.java
@@ -0,0 +1,33 @@
+package com.neo4j.docker.utils;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.nio.file.Files;
+
+/**
+ * HttpHandler that responds to all hhtp requests with the given file from the file system
+ */
+public class HostFileHttpHandler implements HttpHandler
+{
+    private final File file;
+    private final String contentType;
+
+    public HostFileHttpHandler( File fileToDownload, String contentType )
+    {
+        this.file = fileToDownload;
+        this.contentType = contentType;
+    }
+
+    @Override
+    public void handle( HttpExchange exchange ) throws IOException
+    {
+        exchange.getResponseHeaders().add( "Content-Type", contentType );
+        exchange.sendResponseHeaders( HttpURLConnection.HTTP_OK, file.length() );
+        Files.copy( this.file.toPath(), exchange.getResponseBody() );
+        exchange.close();
+    }
+}
