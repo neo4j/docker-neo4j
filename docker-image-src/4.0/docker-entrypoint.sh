a18c1b8c8e9275313639a6aa6737af1eb3f912d2
Bledi Feshti <bledifeshti@neotechnology.com>
2019-09-11 09:22:28 +0200

    docker-entrypoint will now set default configs based on Neo4j Edition

--- docker-image-src/4.0/docker-entrypoint.sh
+++ docker-image-src/4.0/docker-entrypoint.sh
@@ -226,16 +226,16 @@ fi
 : ${NEO4J_dbms_unmanaged__extension__classes:=${NEO4J_dbms_unmanagedExtensionClasses:-}}
 : ${NEO4J_dbms_allow__format__migration:=${NEO4J_dbms_allowFormatMigration:-}}
 : ${NEO4J_dbms_connectors_default__advertised__address:=${NEO4J_dbms_connectors_defaultAdvertisedAddress:-}}
-: ${NEO4J_ha_server__id:=${NEO4J_ha_serverId:-}}
-: ${NEO4J_ha_initial__hosts:=${NEO4J_ha_initialHosts:-}}
-: ${NEO4J_causal__clustering_expected__core__cluster__size:=${NEO4J_causalClustering_expectedCoreClusterSize:-}}
-: ${NEO4J_causal__clustering_initial__discovery__members:=${NEO4J_causalClustering_initialDiscoveryMembers:-}}
-: ${NEO4J_causal__clustering_discovery__listen__address:=${NEO4J_causalClustering_discoveryListenAddress:-"0.0.0.0:5000"}}
-: ${NEO4J_causal__clustering_discovery__advertised__address:=${NEO4J_causalClustering_discoveryAdvertisedAddress:-"$(hostname):5000"}}
-: ${NEO4J_causal__clustering_transaction__listen__address:=${NEO4J_causalClustering_transactionListenAddress:-"0.0.0.0:6000"}}
-: ${NEO4J_causal__clustering_transaction__advertised__address:=${NEO4J_causalClustering_transactionAdvertisedAddress:-"$(hostname):6000"}}
-: ${NEO4J_causal__clustering_raft__listen__address:=${NEO4J_causalClustering_raftListenAddress:-"0.0.0.0:7000"}}
-: ${NEO4J_causal__clustering_raft__advertised__address:=${NEO4J_causalClustering_raftAdvertisedAddress:-"$(hostname):7000"}}
+
+if [ "${NEO4J_EDITION}" == "enterprise" ];
+  then
+   : ${NEO4J_causal__clustering_expected__core__cluster__size:=${NEO4J_causalClustering_expectedCoreClusterSize:-}}
+   : ${NEO4J_causal__clustering_initial__discovery__members:=${NEO4J_causalClustering_initialDiscoveryMembers:-}}
+   : ${NEO4J_causal__clustering_discovery__advertised__address:=${NEO4J_causalClustering_discoveryAdvertisedAddress:-"$(hostname):5000"}}
+   : ${NEO4J_causal__clustering_transaction__advertised__address:=${NEO4J_causalClustering_transactionAdvertisedAddress:-"$(hostname):6000"}}
+   : ${NEO4J_causal__clustering_raft__advertised__address:=${NEO4J_causalClustering_raftAdvertisedAddress:-"$(hostname):7000"}}
+fi
+
 
 # unset old hardcoded unsupported env variables
 unset NEO4J_dbms_txLog_rotation_retentionPolicy NEO4J_UDC_SOURCE \
@@ -252,11 +252,13 @@ unset NEO4J_dbms_txLog_rotation_retentionPolicy NEO4J_UDC_SOURCE \
 
 # Custom settings for dockerized neo4j
 : ${NEO4J_wrapper_java_additional:=-Dneo4j.ext.udc.source=docker}
-: ${NEO4J_ha_host_coordination:=$(hostname):5001}
-: ${NEO4J_ha_host_data:=$(hostname):6001}
-: ${NEO4J_causal__clustering_discovery__advertised__address:=$(hostname):5000}
-: ${NEO4J_causal__clustering_transaction__advertised__address:=$(hostname):6000}
-: ${NEO4J_causal__clustering_raft__advertised__address:=$(hostname):7000}
+
+if [ "${NEO4J_EDITION}" == "enterprise" ];
+  then
+   : ${NEO4J_causal__clustering_discovery__advertised__address:=$(hostname):5000}
+   : ${NEO4J_causal__clustering_transaction__advertised__address:=$(hostname):6000}
+   : ${NEO4J_causal__clustering_raft__advertised__address:=$(hostname):7000}
+fi
 
 if [ -d /conf ]; then
     if secure_mode_enabled; then
@@ -333,25 +335,40 @@ if [ "${cmd}" == "neo4j" ]; then
     fi
 fi
 
-declare -A CONFIG
+declare -A COMMUNITY
+declare -A ENTERPRISE
 
-CONFIG=(
+COMMUNITY=(
      [dbms.tx_log.rotation.retention_policy]="100M size"
      [dbms.memory.pagecache.size]="512M"
      [dbms.connectors.default_listen_address]="0.0.0.0"
      [dbms.connector.https.listen_address]="0.0.0.0:7473"
      [dbms.connector.http.listen_address]="0.0.0.0:7474"
      [dbms.connector.bolt.listen_address]="0.0.0.0:7687"
+)
+
+ENTERPRISE=(
      [causal_clustering.transaction_listen_address]="0.0.0.0:6000"
      [causal_clustering.raft_listen_address]="0.0.0.0:7000"
      [causal_clustering.discovery_listen_address]="0.0.0.0:5000"
 )
 
-for conf in ${!CONFIG[@]} ; do
+for conf in ${!COMMUNITY[@]} ; do
 
     if ! grep -q "^$conf" "${NEO4J_HOME}"/conf/neo4j.conf
     then
-        echo -e "\n"$conf=${CONFIG[$conf]} >> "${NEO4J_HOME}"/conf/neo4j.conf
+        echo -e "\n"$conf=${COMMUNITY[$conf]} >> "${NEO4J_HOME}"/conf/neo4j.conf
+    fi
+done
+
+for conf in ${!ENTERPRISE[@]} ; do
+
+    if [ "${NEO4J_EDITION}" == "enterprise" ];
+    then
+       if ! grep -q "^$conf" "${NEO4J_HOME}"/conf/neo4j.conf
+       then
+        echo -e "\n"$conf=${ENTERPRISE[$conf]} >> "${NEO4J_HOME}"/conf/neo4j.conf
+       fi
     fi
 done
 
