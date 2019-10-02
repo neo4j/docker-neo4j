35583e9a7a9edd5232e1b76e9668eb61d8575bf7
Jenny <jenny.owen@neo4j.com>
2019-10-02 15:22:43 +0200

    improved Neo4j version parser and muted PluginInstallation test when running against SNAPSHOT versions.

--- src/test/java/com/neo4j/docker/utils/Neo4jVersion.java
+++ src/test/java/com/neo4j/docker/utils/Neo4jVersion.java
@@ -21,7 +21,7 @@ public class Neo4jVersion
         // Could be one of the forms:
         // A.B.C, A.B.C-alphaDD, A.B.C-betaDD, A.B.C-rcDD
         // (?<major>\d)\.(?<minor>\d)\.(?<patch>[\d]+)(?<label>-(alpha|beta|[Rr][Cc])[\d]{1,2})?
-        Pattern pattern = Pattern.compile( "(?<major>\\d)\\.(?<minor>\\d)\\.(?<patch>[\\d]+)(?<label>-(alpha|beta|[Rr][Cc])[\\d]{1,2})?" );
+        Pattern pattern = Pattern.compile( "(?<major>\\d)\\.(?<minor>\\d)\\.(?<patch>[\\d]+)(?<label>-(alpha[\\d]{1,2}|beta[\\d]{1,2}|[Rr][Cc][\\d]{1,2}|SNAPSHOT))?" );
         Matcher x = pattern.matcher( version );
         x.find();
         return new Neo4jVersion(
