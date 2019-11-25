1e47370c9cd405c04c0a6d7c2e3ef4e02c54f0e4
Bledi Feshti <bledifeshti@neotechnology.com>
2019-11-25 12:48:33 +0100

    fixed indentation and added some helpful comments

--- docker-image-src/3.4/docker-entrypoint.sh
+++ docker-image-src/3.4/docker-entrypoint.sh
@@ -369,15 +369,16 @@ for conf in ${!ENTERPRISE[@]} ; do
     fi
 done
 
-    if grep -q "udc.source=tarball" "${NEO4J_HOME}"/conf/neo4j.conf
-     then
-      sed -i -e 's/udc.source=tarball/udc.source=docker/g' "${NEO4J_HOME}"/conf/neo4j.conf
-    fi
-
-    if ! grep -q "dbms.jvm.additional=-Dunsupported.dbms.udc.source=docker" "${NEO4J_HOME}"/conf/neo4j.conf
-     then
-      sed -i -e 's/dbms.jvm.additional=/dbms.jvm.additional=-Dunsupported.dbms.udc.source=docker,/g' "${NEO4J_HOME}"/conf/neo4j.conf
-    fi
+#The udc.source=tarball should be replaced by udc.source=docker in both dbms.jvm.additional and wrapper.java.additional
+#Using sed to replace only this part will allow the custom configs to be added after, separated by a ,.
+if grep -q "udc.source=tarball" "${NEO4J_HOME}"/conf/neo4j.conf; then
+     sed -i -e 's/udc.source=tarball/udc.source=docker/g' "${NEO4J_HOME}"/conf/neo4j.conf
+fi
+#The udc.source should always be set to docker by default and we have to allow also custom configs to be added after that.
+#In this case, this piece of code helps to add the default value and a , to support custom configs after.
+if ! grep -q "dbms.jvm.additional=-Dunsupported.dbms.udc.source=docker" "${NEO4J_HOME}"/conf/neo4j.conf; then
+  sed -i -e 's/dbms.jvm.additional=/dbms.jvm.additional=-Dunsupported.dbms.udc.source=docker,/g' "${NEO4J_HOME}"/conf/neo4j.conf
+fi
 
 # list env variables with prefix NEO4J_ and create settings from them
 unset NEO4J_AUTH NEO4J_SHA256 NEO4J_TARBALL
