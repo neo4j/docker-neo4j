1d48defc3deda9c42b17e95a8af2b253963d115d
Jenny <jenny.owen@neo4j.com>
2021-10-21 13:58:30 +0200

    authetication and multiuser tests for dump/load

--- src/test/java/com/neo4j/docker/neo4jadmin/TestDumpLoad.java
+++ src/test/java/com/neo4j/docker/neo4jadmin/TestDumpLoad.java
@@ -21,17 +21,18 @@ import java.util.function.Consumer;
 
 public class TestDumpLoad
 {
-    // test as default user
-    // test as not default user
-    // test with auth
-    // test without auth
-
     private static Logger log = LoggerFactory.getLogger( TestDumpLoad.class );
 
-    private GenericContainer createDBContainer( boolean asDefaultUser )
+    private GenericContainer createDBContainer( boolean asDefaultUser, String password )
     {
+        String auth = "none";
+        if(!password.equalsIgnoreCase("none"))
+        {
+            auth = "neo4j/"+password;
+        }
+
         GenericContainer container = new GenericContainer( TestSettings.IMAGE_ID );
-        container.withEnv( "NEO4J_AUTH", "none" )
+        container.withEnv( "NEO4J_AUTH", auth )
                  .withEnv( "NEO4J_ACCEPT_LICENSE_AGREEMENT", "yes" )
                  .withExposedPorts( 7474, 7687 )
                  .withLogConsumer( new Slf4jLogConsumer( log ) )
@@ -54,8 +55,7 @@ public class TestDumpLoad
     private GenericContainer createAdminContainer( boolean asDefaultUser )
     {
         GenericContainer container = new GenericContainer( TestSettings.ADMIN_IMAGE_ID );
-        container.withEnv( "NEO4J_AUTH", "none" )
-                 .withEnv( "NEO4J_ACCEPT_LICENSE_AGREEMENT", "yes" )
+        container.withEnv( "NEO4J_ACCEPT_LICENSE_AGREEMENT", "yes" )
                  .withExposedPorts( 7474, 7687 )
                  .withLogConsumer( new Slf4jLogConsumer( log ) )
                  .waitingFor( new LogMessageWaitStrategy().withRegEx( "^Done: \\d+ files, [\\d\\.,]+[KMGi]+B processed\\..*" ) )
@@ -69,15 +69,27 @@ public class TestDumpLoad
     }
 
     @Test
-    void shouldDumpAndLoad_defaultUser() throws Exception
+    void shouldDumpAndLoad_defaultUser_noAuth() throws Exception
     {
-        shouldCreateDumpAndLoadDump( true );
+        shouldCreateDumpAndLoadDump( true, "none" );
     }
 
     @Test
-    void shouldDumpAndLoad_nonDefaultUser() throws Exception
+    void shouldDumpAndLoad_nonDefaultUser_noAuth() throws Exception
     {
-        shouldCreateDumpAndLoadDump( false );
+        shouldCreateDumpAndLoadDump( false, "none" );
+    }
+
+    @Test
+    void shouldDumpAndLoad_defaultUser_withAuth() throws Exception
+    {
+        shouldCreateDumpAndLoadDump( true, "verysecretpassword" );
+    }
+
+    @Test
+    void shouldDumpAndLoad_nonDefaultUser_withAuth() throws Exception
+    {
+        shouldCreateDumpAndLoadDump( false, "verysecretpassword" );
     }
 
     //container.stop() actually runs the killContainer Command, preventing clean shutdown.
@@ -87,7 +99,7 @@ public class TestDumpLoad
         container.getDockerClient().stopContainerCmd( container.getContainerId() ).exec();
     }
 
-    private void shouldCreateDumpAndLoadDump( boolean asDefaultUser ) throws Exception
+    private void shouldCreateDumpAndLoadDump( boolean asDefaultUser, String password ) throws Exception
     {
         Path testOutputFolder = HostFileSystemOperations.createTempFolder( "dumpandload-" );
         Path firstDataDir;
@@ -95,13 +107,13 @@ public class TestDumpLoad
         Path backupDir;
 
         // start a database and populate it
-        try(GenericContainer container = createDBContainer( asDefaultUser ))
+        try(GenericContainer container = createDBContainer( asDefaultUser, password ))
         {
             firstDataDir = HostFileSystemOperations.createTempFolderAndMountAsVolume(
                     container, "data1-", "/data", testOutputFolder );
             container.start();
             DatabaseIO dbio = new DatabaseIO( container );
-            dbio.putInitialDataIntoContainer( "neo4j", "none" );
+            dbio.putInitialDataIntoContainer( "neo4j", password );
             stopContainer( container );
         }
 
@@ -128,12 +140,12 @@ public class TestDumpLoad
         }
 
         // verify data in 2nd data directory by starting a database and verifying data we populated earlier
-        try(GenericContainer container = createDBContainer( asDefaultUser ))
+        try(GenericContainer container = createDBContainer( asDefaultUser, password ))
         {
             HostFileSystemOperations.mountHostFolderAsVolume( container, secondDataDir, "/data" );
             container.start();
             DatabaseIO dbio = new DatabaseIO( container );
-            dbio.verifyDataInContainer( "neo4j", "none" );
+            dbio.verifyDataInContainer( "neo4j", password );
         }
     }
 }
