c6afec46ac6004329b00886704e66c1a18bb6fc5
Stefanos Giagkiozis <SteveGiagkiozis@gmail.com>
2024-08-29 11:12:21 +0100

    refactor

--- src/test/java/com/neo4j/docker/TestDockerComposeSecrets.java
+++ src/test/java/com/neo4j/docker/TestDockerComposeSecrets.java
@@ -7,7 +7,7 @@ import org.testcontainers.containers.DockerComposeContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 
 import java.io.File;
-import java.io.InputStream;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -24,17 +24,13 @@ public class TestDockerComposeSecrets
     @RegisterExtension
     public static TemporaryFolderManager temporaryFolderManager = new TemporaryFolderManager();
 
-    String dockerComposeFileName = "simple-container-compose.yml";
-
     @Test
     void shouldCreateContainer() throws Exception
     {
-        Path tmpDir = temporaryFolderManager.createFolder( "Simple_Container_Compose_" );
+        var tmpDir = temporaryFolderManager.createFolder( "Simple_Container_Compose_" );
+        var composeFile = copyDockerComposeResourceFile( tmpDir, "simple-container-compose.yml" );
 
-        File compose_file = new File( tmpDir.toString(), dockerComposeFileName );
-        Files.copy( Objects.requireNonNull( getClass().getClassLoader().getResourceAsStream( dockerComposeFileName ) ), Paths.get( compose_file.getPath() ) );
-
-        var dockerComposeContainer = new DockerComposeContainer( compose_file )
+        var dockerComposeContainer = new DockerComposeContainer( composeFile )
                 .withExposedService( "simplecontainer", DEFAULT_BOLT_PORT )
                 .withExposedService( "simplecontainer", DEFAULT_HTTP_PORT,
                                      Wait.forHttp( "/" )
@@ -43,9 +39,16 @@ public class TestDockerComposeSecrets
                                          .withStartupTimeout( Duration.ofSeconds( 300 ) ) )
                 .withEnv( "NEO4J_IMAGE", getenv( "NEO4J_IMAGE" ) )
                 .withEnv( "HOST_ROOT", tmpDir.toAbsolutePath().toString() )
-                .withEnv( "BOLT_PORT", Integer.toString( DEFAULT_BOLT_PORT) )
-                .withEnv( "HTTP_PORT", Integer.toString( DEFAULT_HTTP_PORT) );
+                .withEnv( "BOLT_PORT", Integer.toString( DEFAULT_BOLT_PORT ) )
+                .withEnv( "HTTP_PORT", Integer.toString( DEFAULT_HTTP_PORT ) );
 
         dockerComposeContainer.start();
+    }
+
+    private File copyDockerComposeResourceFile( Path targetDirectory, String fileName ) throws IOException
+    {
+        File compose_file = new File( targetDirectory.toString(), fileName );
+        Files.copy( Objects.requireNonNull( getClass().getClassLoader().getResourceAsStream( fileName ) ), Paths.get( compose_file.getPath() ) );
+        return compose_file;
     }
 }
