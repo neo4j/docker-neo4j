b23e435e1c0809c01d00aecf4732a768dcc0e933
Tobias Ericsson <tobias.ericsson@neo4j.com>
2024-11-04 11:09:26 +0100

    Changing startup timeout from 150 to 200 seconds

--- src/test/java/com/neo4j/docker/utils/WaitStrategies.java
+++ src/test/java/com/neo4j/docker/utils/WaitStrategies.java
@@ -16,7 +16,7 @@ import org.testcontainers.utility.DockerStatus;
 public class WaitStrategies
 {
 
-    private final static Duration STARTUP_TIMEOUT_SECONDS = Duration.ofSeconds(150);
+    private final static Duration STARTUP_TIMEOUT_SECONDS = Duration.ofSeconds(200);
 
     private WaitStrategies() {}
 
