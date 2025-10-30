b29a5c45e6b92dca2da2723e176b5bcb5aef2957
Jenny Owen <jenny.owen@neo4j.com>
2025-10-30 13:43:43 +0000

    publish neo4j admin scripts need updating to name images after real base os not just 'debian' (#565)

--- publish-neo4j-admin-images.sh
+++ publish-neo4j-admin-images.sh
@@ -22,8 +22,8 @@ docker buildx imagetools create "${REPOSITORY}":5-community \
 echo "Publishing ${REPOSITORY}:${NEO4JVERSION}"
 "${ROOT_DIR}/publish-neo4j-admin-image.sh" "${NEO4JVERSION}" "enterprise" "ubi9" "${REPOSITORY}"
 "${ROOT_DIR}/publish-neo4j-admin-image.sh" "${NEO4JVERSION}" "community" "ubi9" "${REPOSITORY}"
-"${ROOT_DIR}/publish-neo4j-admin-image.sh" "${NEO4JVERSION}" "enterprise" "debian" "${REPOSITORY}"
-"${ROOT_DIR}/publish-neo4j-admin-image.sh" "${NEO4JVERSION}" "community" "debian" "${REPOSITORY}"
+"${ROOT_DIR}/publish-neo4j-admin-image.sh" "${NEO4JVERSION}" "enterprise" "bullseye" "${REPOSITORY}"
+"${ROOT_DIR}/publish-neo4j-admin-image.sh" "${NEO4JVERSION}" "community" "bullseye" "${REPOSITORY}"
 
 echo "Adding extra tags..."
 
@@ -31,32 +31,50 @@ MAJOR=$(get_major_from_version "${NEO4JVERSION}")
 
 if [[ "$MAJOR" == "5" ]]; then
     echo "Tagging ${MAJOR}..."
-    docker buildx imagetools create "${REPOSITORY}:${NEO4JVERSION}-community-debian" \
-    --tag "${REPOSITORY}:5-community" \
-    --tag "${REPOSITORY}:5"
+    docker buildx imagetools create "${REPOSITORY}:${NEO4JVERSION}-community-bullseye" \
+    --tag "${REPOSITORY}:${MAJOR}-community-bullseye" \
+    --tag "${REPOSITORY}:${MAJOR}-community-debian" \
+    --tag "${REPOSITORY}:${MAJOR}-community" \
+    --tag "${REPOSITORY}:${MAJOR}"
 
-    docker buildx imagetools create "${REPOSITORY}:${NEO4JVERSION}-enterprise-debian" \
-    --tag "${REPOSITORY}:5-enterprise"
+    docker buildx imagetools create "${REPOSITORY}:${NEO4JVERSION}-enterprise-bullseye" \
+    --tag "${REPOSITORY}:${MAJOR}-enterprise-bullseye" \
+    --tag "${REPOSITORY}:${MAJOR}-enterprise-debian" \
+    --tag "${REPOSITORY}:${MAJOR}-enterprise"
 
 elif [[ "$MAJOR" -gt 2024 ]]; then
     echo "Tagging calver ${MAJOR}..."
-    docker buildx imagetools create "${REPOSITORY}:${NEO4JVERSION}-community-debian" \
-    --tag "${REPOSITORY}:${MAJOR}" \
+    docker buildx imagetools create "${REPOSITORY}:${NEO4JVERSION}-community-bullseye" \
+    --tag "${REPOSITORY}:${MAJOR}-community-bullseye" \
+    --tag "${REPOSITORY}:${MAJOR}-community-debian" \
     --tag "${REPOSITORY}:${MAJOR}-community" \
+    --tag "${REPOSITORY}:${MAJOR}" \
+    --tag "${REPOSITORY}:community-bullseye" \
     --tag "${REPOSITORY}:community-debian" \
     --tag "${REPOSITORY}:community" \
+    --tag "${REPOSITORY}:bullseye" \
     --tag "${REPOSITORY}:debian" \
     --tag "${REPOSITORY}:latest"
 
     docker buildx imagetools create "${REPOSITORY}:${NEO4JVERSION}-community-ubi9" \
+    --tag "${REPOSITORY}:${MAJOR}-community-ubi9" \
+    --tag "${REPOSITORY}:${MAJOR}-community-redhat" \
     --tag "${REPOSITORY}:community-ubi9" \
+    --tag "${REPOSITORY}:community-redhat" \
     --tag "${REPOSITORY}:ubi9" \
+    --tag "${REPOSITORY}:redhat"
 
-    docker buildx imagetools create "${REPOSITORY}:${NEO4JVERSION}-enterprise-debian" \
+    docker buildx imagetools create "${REPOSITORY}:${NEO4JVERSION}-enterprise-bullseye" \
+    --tag "${REPOSITORY}:${MAJOR}-enterprise-bullseye" \
+    --tag "${REPOSITORY}:${MAJOR}-enterprise-debian" \
     --tag "${REPOSITORY}:${MAJOR}-enterprise" \
+    --tag "${REPOSITORY}:enterprise-bullseye" \
     --tag "${REPOSITORY}:enterprise-debian" \
     --tag "${REPOSITORY}:enterprise"
 
     docker buildx imagetools create "${REPOSITORY}:${NEO4JVERSION}-enterprise-ubi9" \
-    --tag "${REPOSITORY}:enterprise-ubi9"
+    --tag "${REPOSITORY}:${MAJOR}-enterprise-ubi9" \
+    --tag "${REPOSITORY}:${MAJOR}-enterprise-redhat" \
+    --tag "${REPOSITORY}:enterprise-ubi9" \
+    --tag "${REPOSITORY}:enterprise-redhat"
 fi
