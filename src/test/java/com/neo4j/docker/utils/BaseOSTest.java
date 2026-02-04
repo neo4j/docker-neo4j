e81105e8e62682acba2772929f5ecc88c657ad73
Jenny Owen <jenny.owen@neo4j.com>
2026-02-04 12:47:44 +0000

    fixed baseOS test about the deprecation warnings

--- src/test/java/com/neo4j/docker/utils/BaseOSTest.java
+++ src/test/java/com/neo4j/docker/utils/BaseOSTest.java
@@ -30,8 +30,8 @@ class BaseOSTest {
     }
 
     @ParameterizedTest(name = "{0}, {1}.{2}.{3}")
-    @CsvSource({"ubi8,5,20,0", "ubi9,5,26,21", "bullseye,5,26,21",
-                "ubi8,2024,1,0", "ubi9,2026,3,0","bullseye,2026,3,0"
+    @CsvSource({"ubi8,5,20,0", "ubi9,5,26,23", "bullseye,5,26,23",
+                "ubi8,2024,1,0", "ubi9,2026,4,0","bullseye,2026,4,0"
     })
     void testHasDeprecationWarning_equal(String name, int major, int minor, int patch) {
         BaseOS os = BaseOS.fromString(name);
@@ -44,7 +44,7 @@ class BaseOSTest {
 
     @ParameterizedTest(name = "{0}, {1}.{2}.{3}")
     @CsvSource({"ubi8,5,26,0", "ubi9,5,26,50", "bullseye,5,26,50",
-            "ubi8,2027,1,0", "ubi9,2026,4,0", "bullseye,2026,4,0"
+            "ubi8,2027,1,0", "ubi9,2080,4,0", "bullseye,2080,4,0"
     })
     void testHasDeprecationWarning_after(String name, int major, int minor, int patch) {
         BaseOS os = BaseOS.fromString(name);
